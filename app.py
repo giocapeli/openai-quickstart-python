39204d5b4f283fa4510d54e217eb6b218d904570
Giovanni Capeli <capeli.andreas@gmail.com>
2023-03-10 13:39:48 +0100

    eddit

--- app.py
+++ app.py
@@ -23,16 +23,12 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+def generate_prompt(line):
+    return """ Pretend your talking to a friend so answer his question or suggest a new topic:
+Friend:{}
+You:
+""".format(
+        line.capitalize()
     )
 
 if __name__ == '__main__':
